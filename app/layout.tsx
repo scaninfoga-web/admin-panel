d8edbbe2e0d41403a2b4846e449807378b0f86e6
abhinav-2125 <abhinavsrivastava@scaninfoga.in>
2025-07-21 14:15:58 +0530

    admin-panle-v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { ProviderUtil } from '@/redux/provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Next.js 14 + Bun',
-  description: 'Example with Tailwind v3 + shadcn/ui',
+  title: 'Scaninfoga Admin-Panel',
+  description: '',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -17,7 +17,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <ProviderUtil>
-
         <Toaster richColors position="top-right" />
         <Navbar />
         {children}
